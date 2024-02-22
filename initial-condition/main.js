4b654a0efe5cfb5118dad3eecdaf981058d88a10
rayyan21d <rayyan9290@gmail.com>
2024-02-22 23:47:16 +0530

    week - 7 reached

---
+++ initial-condition/main.js
@@ -0,0 +1,122 @@
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
+
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI("AIzaSyAvubAUBG3xVXZZ5GlIMUa3jJA4JMOIKY8");
+
+let chatHistory = []
+
+// async function run() {
+//     // For text-only input, use the gemini-pro model
+//     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+//     const prompt = "Write a story about a magic backpack."
+
+//     const result = await model.generateContent(prompt);
+//     const response = await result.response;
+//     const text = response.text();
+//     console.log(text);
+// }
+
+// run();
+
+/*
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path, mimeType) {
+    return {
+        inlineData: {
+            data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+            mimeType
+        },
+    };
+}
+
+
+async function ImageRun() {
+    // For text-and-image input (multimodal), use the gemini-pro-vision model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+    const prompt = "What's different between these pictures?";
+
+    const imageParts = [
+        fileToGenerativePart("image1.png", "image/png"),
+        fileToGenerativePart("image2.jpeg", "image/jpeg"),
+    ];
+
+    const result = await model.generateContent([prompt, ...imageParts]);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+} 
+
+*/
+
+
+async function run() {
+    // Initialize a modelOne
+    const modelOne = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const chatOne = modelOne.startChat({
+
+        history: [
+            // {
+            //     role: "user",
+            //     parts: "Hello, I have 2 dogs in my house.",
+            // },
+            // {
+            //     role: "model",
+            //     parts: "Great to meet you. What would you like to know?",
+            // },
+        ],
+        // generationConfig: {
+        //     maxOutputTokens: 100,
+        // },
+    });
+
+    const init = "Who are you? Are you conscious?";
+
+    const result = await chatOne.sendMessage(init);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+
+    // Add this text to the history[] and pump it back to modelTwo
+
+    chatHistory.push({
+        role: "user",
+        parts: text
+    })
+
+    // Now pass this history to model2
+    const modelTwo = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const chatTwo = modelTwo.startChat({
+        history: chatHistory
+    })
+
+
+
+
+
+
+
+}
+
+run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
